2fe488f393f59afb6e3f933594a38f083054acbf
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-19 01:04:45 -0700

    Restarting the method

--- components/SwipingDeck.js
+++ components/SwipingDeck.js
@@ -4,47 +4,9 @@ import Swiper from "react-native-deck-swiper";
 
 
 const SwipingDeck = () => {
-    const [pets, setPets] = useState([
-        { name: 'Bella', breed: 'Labrador' },
-        { name: 'Charlie', breed: 'Beagle' },
-        { name: 'Max', breed: 'Golden Retriever' }
-    ]);
-
-    const [likedPets, setLikedPets] = useState([]);
-
-    const [dislikePets, setDislikePets] = useState([]);
-
-    const handleLikedPets = (index) => {
-        console.log("Liked pet", pet[index].name);
-    };
-
-    const handleDislikedPets = (index) => {
-        console.log("Disliked", pet[index].name);
-    };
-
     return (
-        <View style={styles.container}>
-            <Swiper>
-                cards = {pets}
-                renderCard={(pet) => (
-                    <View>
-                        <Text>{pet.name}</Text>
-                        <Text>{pet.breed}</Text>
-                    </View>
-                )}
-                onSwipedRight={handleLikedPets}
-                onSwipedLeft={handleDislikedPets}
-            </Swiper>
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignContent: "center"
-    }
-});
+        <Text>Hello</Text>
+    )
+}
 
 export default SwipingDeck;
