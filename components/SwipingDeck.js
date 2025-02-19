39b792497d368dbafc3470f38a3fad961ba29410
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-19 01:14:54 -0700

    created liked and disliked arrays, created handling liking and disliking pets. created useeffect to console log the array of liked and disliked when an update occurs

--- components/SwipingDeck.js
+++ components/SwipingDeck.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Swiper from "react-native-deck-swiper";
 
 
 const SwipingDeck = () => {
+    const [likedPets, setLikedPets] = useState([]);
+    const [dislikedPets, setDislikedPets] = useState([]);
+
+    const handleLikedPets = (index) => {
+        setLikedPets((prev) => ([...prev, pets[index]]));
+    };
+    
+    const handleDislikedPets = (index) => {
+        setDislikedPets((prev) => ([...prev, pets[index]]));
+    };
+
+    useEffect(() => {
+        console.log("Liked pets: ", likedPets);}, [likedPets]);
+    
+    useEffect(() => {
+        console.log("Disliked pets: ", dislikedPets)}, [dislikedPets]);
+
     return (
         <Text>Hello</Text>
     )
