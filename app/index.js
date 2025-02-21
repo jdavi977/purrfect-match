7c432a0a26fdd8fdbb36c214df45a8758462fba9
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-21 15:33:12 -0700

    Created the card, working on displaying it on index

--- app/index.js
+++ app/index.js
@@ -1,8 +1,19 @@
 import React, {useState, useEffect} from "react";
 import { Text, View, Image } from "react-native";
-import SwipingDeck from "../components/SwipingDeck";
+import {Card} from "../components/Card";
+import {petData as petDataArray} from "../utils/petData";
+
 
 export default function Index() {
+  const [petData, setPetData] = useState(petDataArray);
+
+  // If there are no more pets it will loop back
+  useEffect(() => {
+    if (petData.length === 0) { 
+      setPetData(petDataArray);
+    }
+  }, [petData.length])
+
   return (
     <View
       style={{
--- components/Card.js
+++ components/Card.js
@@ -1,15 +1,47 @@
-import React, {useState} from "react";
+import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 
 
-const Card = () => {
-
-    
+const Card = ({name, age, breed, image}) => {
+    <View stlye={styles.container}>
+        <Image source={image} style={styles.image}/>
+        <LinearGradient
+            color={['transparent', 'rgba(0,0,0,0.9']}
+            style={styles.gradient}
+        >
+        <View style={styles.userContainer}>
+            <Text style={styles.nameText}>{name}, {age}</Text>
+            <Text style={styles.breedText}>{breed}</Text>
+        </View>
+        </LinearGradient>
+    </View>
 }
 
 const styles = StyleSheet.create({
     container: {
 
     },
-})
+    
+    image: {
+        
+    },
+
+    gradient: {
+
+    },
+    userContainer: {
+
+    },
+    nameText: {
+
+    },
+    breedText: {
+
+    },
+
+
+});
+
+export default Card;
