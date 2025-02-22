63a50425e1796d54cbf83dc010f50a71f1226d64
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-22 15:28:55 -0700

    Finished making card, styling, and swiping function

--- app/index.js
+++ app/index.js
@@ -1,18 +1,37 @@
 import React, {useState, useEffect} from "react";
-import { Text, View, Image } from "react-native";
-import {Card} from "../components/Card";
+import { View } from "react-native";
+import Card from "../components/Card.js";
 import {petData as petDataArray} from "../utils/petData";
+import Swiper from "react-native-deck-swiper";
 
 
 export default function Index() {
   const [petData, setPetData] = useState(petDataArray);
+  const [likedPets, setLikedPets] = useState([]);
+  const [dislikedPets, setDislikedPets] = useState([]);
+
+  const handleLiked = (index) => {
+    setLikedPets((prev) => ([...prev, petData[index]]));
+  };
+
+  const handleDisliked = (index) => {
+    setDislikedPets((prev) => ([...prev, petData[index]]));
+  };
+
+  useEffect(() => {
+    console.log("Pets liked: ", likedPets)
+  }, [likedPets]);
+
+  useEffect(() => {
+    console.log("Pets disliked: ", dislikedPets)
+  }, [dislikedPets]);
 
   // If there are no more pets it will loop back
   useEffect(() => {
-    if (petData.length === 0) { 
+    if (!petData.length) { 
       setPetData(petDataArray);
     }
-  }, [petData.length])
+  }, [petData.length]);
 
   return (
     <View
@@ -23,6 +42,24 @@ export default function Index() {
         backgroundColor: "white"
       }}
     >
+      <Swiper 
+        cards = {petData}
+        renderCard = { (card) =>  (
+          <Card 
+            key= {card.name}
+            name = {card.name}
+            age = {card.age}
+            breed = {card.breed}
+            image = {card.image}
+          />
+        )}
+        onSwipedRight = {handleLiked}
+        onSwipedLeft = {handleDisliked}
+        stackSize = {3}
+        cardIndex = {0}
+        infinite
+        backgroundColor="transparent"
+      />
     </View>
   );
 }
--- utils/petData.js
+++ utils/petData.js
@@ -1,17 +1,20 @@
-export const PetData = [
+export const petData = [
     {
+        id: 1,
         name: "Oreo",
         age: 6,
         breed: "Bulldog",
-        image: require("../assets/images/Oreo.png")
+        image: require("../assets/images/bulldog.png")
     },
     {
+        id: 2,
         name: "Perry",
         age: 3,
         breed: "Golden Doodle",
         image: require("../assets/images/Perry.png")
     },
     {
+        id: 3,
         name: "Coco",
         age: 1,
         breed: "Golden Retreiver",
