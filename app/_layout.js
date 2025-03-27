24e2e8c0877a43a3244f7691ed8d2cf185173f51
Jawne3 <johnlowendavid@yahoo.ca>
2025-03-27 03:38:50 -0600

    Made icons and fixed styling for tab bar navigation

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,54 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+
+const ICON_SIZE = 30;
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarInactiveTintColor: "grey",
+        tabBarStyle: {
+          height: 90,
+          paddingBottom: 20,
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 10,
+          fontWeight: "bold",
+          marginTop: 4,
+        }
+      }}
+    >
+      <Tabs.Screen
+       name="index"
+       options={{
+          title: "feed",
+          tabBarIcon: ({ color, size}) => (
+            <FontAwesome name="paw" size={ICON_SIZE} color={color}/>
+          ),
+        }}
+      />
+      <Tabs.Screen
+       name="favourite"
+       options={{
+          title: "Favourites",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="bookmark" size={ICON_SIZE} color={color}/>
+          ),
+          }}
+      />
+      <Tabs.Screen
+       name="profile"
+       options={{
+         title: "Profile",
+         tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="user" size={ICON_SIZE} color={color}/>
+         ),
+        }}
+      />
+    </Tabs>
+  );
 }
+
+
